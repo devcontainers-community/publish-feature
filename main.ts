ed6cde9e1c4773bdd16f2a0ddc4091862f6db06a
Jacob Hummer <jcbhmr@outlook.com>
2023-08-13 19:56:13 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,26 @@
-#!/usr/bin/env -S deno run -A
+#!/usr/bin/env node
 import { readFile } from "node:fs/promises"
 import { existsSync } from "node:fs"
 import process from "node:process"
 import { $ } from "execa"
 import * as core from "@actions/core"
-import { temporaryFile } from 'tempy';
+import { temporaryFile, temporaryWrite } from 'tempy';
 
 const image = core.getInput("image");
-const files = core.getInput("files").split(/,?\s+/g).filter(x => existsSync(x));
+const files = core.getMultilineInput("files").map(x => fglob())
 const repository = core.getInput("repository");
+const githubServerURL = core.getInput("github_server_url");
+
+const devcontainerFeature = await readFile("devcontainer-feature.json")
 
 const config = {
-  mediaType: "application/vnd.devcontainers"
-}
+  mediaType: "application/vnd.devcontainers",
+};
 const annotations = {
-  "$config": {
-    "hello": "world"
-  },
   "$manifest": {
-    "foo": "bar"
-  },
-  "cake.txt": {
-    "fun": "more cream"
+    "com.github.package.type": "devcontainer_collection",
+    "dev.containers.metadata": JSON.stringify(devcontainerFeature),
+    "org.opencontainers.image.source": githubServerURL + "/" + repository,
   }
 }
 
